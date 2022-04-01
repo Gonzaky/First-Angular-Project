e6bdc43d2ba920ffeef0c39087c65a0b79a454d6
FH\goncalo.vasques <goncalo.vasques@future-healthcare.net>
2022-04-01 10:40:57 +0100

    fixed length errors and added log button

--- src/app/components/emoji/emoji.component.ts
+++ src/app/components/emoji/emoji.component.ts
@@ -15,31 +15,56 @@ export class EmojiComponent implements OnInit {
   random: string = '';
   emojiLog: Array<string> = [];
   previousEmoji: any = [];
-
-
-
+  test: string ='';
+  testLog : string[] = [];
   constructor(private emojiGeneratorService: EmojiGeneratorService) {
 
 
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   public generateEmoji(): void {
-    this.emojiGeneratorService.getEmoji().subscribe({next:data => {
-       /* console.log('agora sim', data);*/
-        this.random = data[0].text[Math.floor((Math.random()*data[0].text.length))];
+    this.emojiGeneratorService.getEmoji().subscribe({
+      next: data => {
+        /* console.log('agora sim', data);*/
+        this.random = data[0].text[Math.floor((Math.random() * data[0].text.length))];
         /*console.log('log do random', this.random);*/
-      this.emojiLog.push(this.random)
-      /*  console.log('array de emojis', this.emojiLog)*/
-  for( let i =0 ; i < this.emojiLog.length ; i++){
-    this.previousEmoji = this.emojiLog[i-1]
+        this.emojiLog.push(this.random)
+        /*  console.log('array de emojis', this.emojiLog)*/
+        for (let i = 0; i < this.emojiLog.length; i++) {
+          this.previousEmoji = this.emojiLog[i - 1]
+        }
+
+      }
+    })
+
   }
 
-      }})
+  public getTest() :void {
+    this.emojiGeneratorService.getEmoji().subscribe((data) => {
+      this.test = data[0].text[Math.floor((Math.random() * data[0].text.length))];
+      this.testLog.push(this.test);
+      console.log('testing this.test without {}',this.test)
+      console.log('testing this.testLog without {}',this.testLog)
 
+    } )
   }
 
 
+  /*
+  *  subscribe can have two syntax ex:
+  *  this.emojiGeneratorService.getEmoji().subscribe( {
+  * next: data => {
+  *  data do stuff
+  * }, error: err => { show errs}, complete: finisher => { do stuff ??}
+  * } )
+  * ------- // -----
+  *
+  * CLEANER WAY I THINK:
+  *  this.emojiGeneratorService.getEmoji().subscribe( (data) => { return stuff},
+  *
+  *  */
 
 }
